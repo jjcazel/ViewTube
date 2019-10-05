7de21d38c533adf429566f45245db95d74c878e8
JJCazel <jjcazel@gmail.com>
2019-10-05 14:53:28 -0400

    Commit before heroku push

--- db/migrate/20191005004116_addcolumnsf_namel_name.rb
+++
@@ -1,7 +0,0 @@
-class AddcolumnsfNamelName < ActiveRecord::Migration[5.2]
-  def change
-    add_column :users, :first_name, :string
-    add_column :users, :last_name, :string
-  end
-
-end
--- db/migrate/20191005004849_changecolumnsf_namel_name.rb
+++ db/migrate/20191005004849_changecolumnsf_namel_name.rb
@@ -1,6 +1,6 @@
-class ChangecolumnsfNamelName < ActiveRecord::Migration[5.2]
-  def change
-    change_column_null :users, :first_name, false
-    change_column_null :users, :last_name, false
-  end
-end
+# class ChangecolumnsfNamelName < ActiveRecord::Migration[5.2]
+#   def change
+#     change_column_null :users, :first_name, false
+#     change_column_null :users, :last_name, false
+#   end
+# end
