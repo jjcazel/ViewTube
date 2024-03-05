d5ea3b7e8dc37434c04a3edc8f0d87318e348f80
Jared Cazel <jjcazel@gmail.com>
2024-03-04 21:56:09 -0500

    Update S3 credentials

--- frontend/components/video_upload/video_upload.jsx
+++ frontend/components/video_upload/video_upload.jsx
@@ -1,19 +1,18 @@
-import React from 'react';
-import GreetingContainer from '../greeting/greeting_container'
-import { withRouter } from 'react-router-dom'
-import SidebarContainer from '../Sidebar/sidebar_container';
+import React from "react";
+import GreetingContainer from "../greeting/greeting_container";
+import { withRouter } from "react-router-dom";
+import SidebarContainer from "../Sidebar/sidebar_container";
 
 class VideoUpload extends React.Component {
-
   constructor(props) {
     super(props);
     this.state = {
       video: null,
       fileUrl: null,
-      title: '',
-      description: '',
+      title: "",
+      description: "",
       loading: false,
-    }
+    };
 
     this.handleFile = this.handleFile.bind(this);
     this.cancelUpload = this.cancelUpload.bind(this);
@@ -21,15 +20,14 @@ class VideoUpload extends React.Component {
   }
 
   changeField(field) {
-    return e => this.setState({ [field]: e.target.value });
+    return (e) => this.setState({ [field]: e.target.value });
   }
-
 
   cancelUpload() {
     this.setState({
       video: null,
-      title: '',
-      description: '',
+      title: "",
+      description: "",
     });
   }
 
@@ -39,107 +37,122 @@ class VideoUpload extends React.Component {
 
   addFileToState(files) {
     if (files.length > 1) {
-      this.props.addUploadErrors(['Please upload one file at a time.']);
+      this.props.addUploadErrors(["Please upload one file at a time."]);
     } else {
       let file = files[0];
-      if (file.kind === 'file') file = file.getAsFile();
+      if (file.kind === "file") file = file.getAsFile();
 
-      if (file.type.slice(0, 5) !== 'video') {
-        this.props.addUploadErrors(['Please choose a video file.']);
-    } else {
-      this.uploadFile(file);
+      if (file.type.slice(0, 5) !== "video") {
+        this.props.addUploadErrors(["Please choose a video file."]);
+      } else {
+        this.uploadFile(file);
+      }
     }
   }
-}
 
   uploadFile(file) {
     const fileReader = new FileReader();
-    this.setState({
-      video: file,
-      fileUrl: fileReader.result,
-      title: file.name,
-    });
+    fileReader.readAsDataURL(file);
+
+    fileReader.onload = () => {
+      this.setState({
+        video: file,
+        fileUrl: fileReader.result,
+        title: file.name,
+      });
+    };
+
+    fileReader.onerror = (error) => {
+      console.error("Error reading file:", error);
+    };
   }
 
   submit(e) {
     e.preventDefault();
-    if (this.state.title === '' ||
-      this.state.description === '' ||
-      this.state.file === null ||
-      this.state.loading) {
-
+    if (
+      this.state.title === "" ||
+      this.state.description === "" ||
+      this.state.video === null ||
+      this.state.loading
+    ) {
       return <div>Uploading...</div>;
     }
     this.setState({ loading: true });
-    let test = new FormData();
-    test.append('title', this.state.title);
-    test.append('description', this.state.description);
-    test.append('video', this.state.video);
-
-    this.props.createVideo(test).then(action => {
-      this.props.history.push(`/videos/${Object.keys(action.video)[0]}`)});
+    let uploadValues = new FormData();
+    uploadValues.append("title", this.state.title);
+    uploadValues.append("description", this.state.description);
+    uploadValues.append("video", this.state.video);
+    this.props.createVideo(uploadValues).then((action) => {
+      this.props.history.push(`/videos/${Object.keys(action.video)[0]}`);
+    }).catch(error => console.error(`There was an error: ${error}`));
   }
 
   render() {
     return (
       <>
-      <div className='nav'>
-        <GreetingContainer />
-      </div>
-      <div className="video-upload-main">
-      <div className="side-bar-whole-2"><SidebarContainer /></div>
-        <div className='other-div'>
+        <div className="nav">
+          <GreetingContainer />
+        </div>
+        <div className="video-upload-main">
+          <div className="side-bar-whole-2">
+            <SidebarContainer />
+          </div>
+          <div className="other-div">
             <div className="upload-form-details">
-
-              <form id='upload' className="post-file-form">
-                <label for='file-upload' class='custom-file-upload'>SELECT FILE</label>
-
+              <form id="upload" className="post-file-form">
+                <label htmlFor="file-upload" className="custom-file-upload">
+                  SELECT FILE
+                </label>
                 <input
-                  id='file-upload'
-                  autoComplete='false'
-                  type='file'
+                  id="file-upload"
+                  autoComplete="false"
+                  type="file"
                   onChange={this.handleFile}
-                  className="choose-file-upload" />
-                <input type='text'
-                  placeholder='Title (required)'
-                  onChange={this.changeField('title')}
-                  className='upload-form-field'
+                  className="choose-file-upload"
+                />
+                <input
+                  type="text"
+                  placeholder="Title (required)"
+                  onChange={this.changeField("title")}
+                  className="upload-form-field"
                   value={this.state.title}
-                  id='upload-form-title'>
-                </input>
+                  id="upload-form-title"
+                ></input>
 
-                <textarea type='text'
-                  id='upload-form-description'
-                  onChange={this.changeField('description')}
-                  className='upload-form-field'
-                  placeholder='Description (required)'
+                <textarea
+                  type="text"
+                  id="upload-form-description"
+                  onChange={this.changeField("description")}
+                  className="upload-form-field"
+                  placeholder="Description (required)"
                   value={this.state.description}
-                  rows='4'>
-                </textarea>
-
-
+                  rows="4"
+                ></textarea>
+                {this.state.loading && <div>Uploading...</div>}
                 <div className="upload-buttons">
-                  <button className="next"
-                    onClick={this.submit}
-                    className="upload-submit">SUBMIT</button>
-                  <button onClick={this.cancelUpload}
-                    id='cancel'
-                    className="cancel">CANCEL</button>
-                <div className='upload-errors'>
-                  {this.props.uploadErrors}
-                  {this.props.otherErrors}
-                  {this.submit}
-                </div>
+                  <button onClick={this.submit} className="upload-submit">
+                    SUBMIT
+                  </button>
+                  <button
+                    onClick={this.cancelUpload}
+                    id="cancel"
+                    className="cancel"
+                  >
+                    CANCEL
+                  </button>
+                  <div className="upload-errors">
+                    {this.props.uploadErrors}
+                    {this.props.otherErrors}
+                    {this.submit}
+                  </div>
                 </div>
               </form>
             </div>
-        </div>
-          
+          </div>
         </div>
       </>
-    )
+    );
   }
-
 }
 
 export default withRouter(VideoUpload);
--- frontend/util/video_api_util.js
+++ frontend/util/video_api_util.js
@@ -1,54 +1,48 @@
-
-export const fetchVideos = search => {
-
+export const fetchVideos = (search) => {
   const data = {};
-  if (search && search.search !== '') {
+  if (search && search.search !== "") {
     Object.assign(data, search);
   }
 
   return $.ajax({
-    method: 'GET',
-    url: '/api/videos',
+    method: "GET",
+    url: "/api/videos",
     data,
   });
 };
 
-export const fetchVideo = id => (
+export const fetchVideo = (id) =>
   $.ajax({
-    method: 'GET',
-    url: `/api/videos/${id}`
-  })
-)
+    method: "GET",
+    url: `/api/videos/${id}`,
+  });
 
 export const addView = (id) => {
   return $.ajax({
-    method: 'POST',
+    method: "POST",
     url: `/api/videos/${id}/views`,
   });
 };
 
-export const addLikeOrDislike = data => {
-
+export const addLikeOrDislike = (data) => {
   const id = data.videoId;
   const is_dislike = {
-    like: { is_dislike: data.isDislike }
+    like: { is_dislike: data.isDislike },
   };
 
   return $.ajax({
-    method: 'POST',
+    method: "POST",
     url: `/api/videos/${id}/likes`,
     data: is_dislike,
   });
 };
 
-export const createVideo = data => {
+export const createVideo = (data) => {
   return $.ajax({
-    method: 'POST',
+    method: "POST",
     url: `/api/videos`,
     data: data,
     contentType: false,
-    processData: false
+    processData: false,
   });
 };
-
-
